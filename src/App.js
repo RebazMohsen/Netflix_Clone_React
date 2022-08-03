5a60594737bff9ca97ddeb5d760924628032f54a
RebazMohsen <Rebazmohsen1111@gmail.com>
2022-08-03 14:59:43 +0300

    Add: Tailwind to our dependency tree...

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div>Hello CornPoppers</div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
