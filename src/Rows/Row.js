7334fd4ec71938d964112cbcb78aab765cd3108e
RebazMohsen <Rebazmohsen1111@gmail.com>
2022-08-19 15:45:49 +0800

    Add : Rows of movies

--- src/Rows/Row.js
+++ src/Rows/Row.js
@@ -32,11 +32,6 @@ function Row({ title, fetchMovie }) {
   return (
     <div className=" pt-[31px] ">
       <div className="pl-[60px]">
-        {/*    <div>
-          {movies.map((item) => {
-            <div key={item}>{JSON.stringify(item.movies.backdrop_path)}</div>;
-          })}
-        </div> */}
         <div>
           <h1>{title}</h1>
         </div>
@@ -46,17 +41,11 @@ function Row({ title, fetchMovie }) {
               key={id}
               src={
                 movies &&
-                `https://image.tmdb.org/t/p/original${movies.backdrop_path} `
+                `https://image.tmdb.org/t/p/original${item.backdrop_path} `
               }
               alt="backdrop"
             />
           ))}
-
-          {/* <img
-            className="onHover hover:shadow-2xl transition-all duration-700  w-[219px]  h-[121px] mt-[15px]  mr-6 "
-            src="https://images.unsplash.com/photo-1615859131861-052f0641a60e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1289&q=80"
-            alt="backdrop_image"
-          /> */}
         </div>
       </div>
     </div>
