725a8c06eb9b8e1ae9d6fa3d6a70d9d3fcb109da
RebazMohsen <Rebazmohsen1111@gmail.com>
2022-09-10 13:13:35 +0300

    add dependerncy : React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import Home from "./Home";
+import Profile from "./Profile";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Profile.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Profile() {
+  return <div>This is Profile from React Router Dom</div>;
+}
+
+export default Profile;
